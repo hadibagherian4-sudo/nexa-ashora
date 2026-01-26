dd817e350c4350d6939b8b5044cd6761e70b6ad2
hadi <hadibagherian4@gmail.com>
2026-01-26 04:36:26 -0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,15 +1,9 @@
 # app/config.py
 import os
 
-DB_PATH = os.getenv("NEXA_DB_PATH", "nexa.db")
+COOKIE_NAME = os.getenv("COOKIE_NAME", "nexa_session")
+COOKIE_SECURE = os.getenv("COOKIE_SECURE", "0") == "1"
 
-JWT_SECRET = os.getenv("NEXA_JWT_SECRET", "CHANGE_ME_SUPER_SECRET")
-JWT_ALG = "HS256"
-COOKIE_NAME = "nexa_token"
-COOKIE_SECURE = os.getenv("NEXA_COOKIE_SECURE", "0") == "1"  # روی HTTPS بذار 1
-
-# Manager credentials (ENV)
-MANAGER_PHONE = os.getenv("NEXA_MANAGER_PHONE", "")
-MANAGER_NID = os.getenv("NEXA_MANAGER_NID", "")
-MANAGER_PASSWORD = os.getenv("NEXA_MANAGER_PASSWORD", "")
-
+MANAGER_PHONE = os.getenv("MANAGER_PHONE", "09146862029")
+MANAGER_NID = os.getenv("MANAGER_NID", "136232506")
+MANAGER_PASSWORD = os.getenv("MANAGER_PASSWORD", "Hadi136236")
