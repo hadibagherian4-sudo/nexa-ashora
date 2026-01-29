232a5203d17620eacc26d1a6621eb8f804382bc5
hadi <hadibagherian4@gmail.com>
2026-01-28 23:20:47 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,20 @@ from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from .config import settings
+from .paths import STATIC_DIR
 from .db import engine
 from .models import Base
 
 from .routers import auth, home, content, forum, profile, admin
+
 
 def create_app():
     app = FastAPI(title=settings.APP_NAME)
 
     app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_SECRET)
 
-    app.mount("/static", StaticFiles(directory="app/static"), name="static")
+    # absolute path for static
+    app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 
     Base.metadata.create_all(bind=engine)
 
@@ -25,5 +28,6 @@ def create_app():
     app.include_router(admin.router)
 
     return app
+
 
 app = create_app()
