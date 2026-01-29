f80f9d66f9da26f8d6e51253a86316b9254c0754
hadi <hadibagherian4@gmail.com>
2026-01-28 23:23:50 -0800

    Update profile.py

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from ..deps import require_login, status_fa
+
+from ..paths import TEMPLATES_DIR
+from ..deps import require_login
 
 router = APIRouter(prefix="/profile", tags=["profile"])
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+
 
 @router.get("")
 def profile(request: Request, auth=Depends(require_login)):
-    return templates.TemplateResponse("profile.html", {"request": request, "auth": auth, "status_fa": status_fa})
+    return templates.TemplateResponse("profile.html", {"request": request, "auth": auth})
